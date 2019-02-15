256d4a360bd8cd6963bce74a1ed8e5dc5ed39bdb
Student <example@example.com>
2019-02-15 09:31:29 -0800

    add css grid and footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './lib/Header';
 import Landing from './lib/Landing';
+import Footer from './lib/Footer';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,13 +13,25 @@ const App = () => {
   return (
 
   <div className="App">
-    <Header />
-      <hr />
-    <Switch>
+    <div className="wrapper">
+
+      <div>
+        <Header />
+        <hr />
+      </div>
+
+
+      <Switch>
       <Route exact path='/' component={Landing} />
+      </Switch>
 
-    </Switch>
 
+      <div>
+        <hr />
+        <Footer />
+      </div>
+
+    </div>
   </div>
 
 
---
+++ src/lib/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer(){
+  return (
+    <div>
+      <h1>funky lil footer</h1>
+
+    </div>
+  );
+}
+
+export default Footer;
