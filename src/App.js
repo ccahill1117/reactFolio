f8234c090f63b6f6d10ffc00550f108021fb97d8
Student <example@example.com>
2019-02-15 10:38:09 -0800

    adds css grid to main page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from './lib/Landing';
 import Footer from './lib/Footer';
 import Background from './lib/Background';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
 
@@ -16,20 +17,19 @@ const App = () => {
   <div className="App">
     <div className="wrapper">
 
-      <div>
+      <div className="header">
         <Header />
         <hr />
       </div>
 
+      <div className="switch">
+        <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/background' component={Background} />
+        </Switch>
+      </div>
 
-      <Switch>
-      <Route exact path='/' component={Landing} />
-      <Route exact path='/background' component={Background} />
-
-      </Switch>
-
-
-      <div>
+      <div className="footer">
         <hr />
         <Footer />
       </div>
