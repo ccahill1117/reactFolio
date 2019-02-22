140d7aaa2a06d08bf0d0fbd698096299bf888394
Christopher Cahill <cthomascahill@gmail.com>
2019-02-22 08:25:28 -0800

    reformat area for friends to leave messages

--- src/lib/Fun.jsx
+++ src/lib/Fun.jsx
@@ -11,63 +11,6 @@ import Moment from 'moment';
 class Fun extends React.Component {
 
 
-  constructor(props) {
-  super(props);
-  this.state = {
-    masterItemList: []
-  };
-  this.handleAddingNewItemToList = this.handleAddingNewItemToList.bind(this);
-}
-
- componentWillUnmount(){
-   console.log('componentWillUnmount');
-   clearInterval(this.waitTimeUpdateTimer);
- }
-
- componentWillMount() {
-   console.log('componentWillMount');
- }
-
- componentWillReceiveProps() {
-   console.log('componentWillReceiveProps');
- }
-
- shouldComponentUpdate() {
-   console.log('shouldComponentUpdate');
-   return true;
- }
-
- componentWillUpdate() {
-   console.log('componentWillUpdate');
- }
-
- componentDidUpdate() {
-   console.log('componentDidUpdate');
- }
-
-componentDidMount() {
-  console.log('hey');
-  this.waitTimeUpdateTimer = setInterval(() =>
-    this.updateItemElapsedWaitTime(),
-    1000
-  );
-}
-
-updateItemElapsedWaitTime() {
-   console.log("check");
-   let newMasterItemList = this.state.masterItemList.slice();
-   newMasterItemList.forEach((item) =>
-     item.formattedWaitTime = (item.timeCreated).fromNow(true)
-   );
-   this.setState({masterItemList: newMasterItemList})
- }
-
-handleAddingNewItemToList(newItem){
-  var newMasterItemList = this.state.masterItemList.slice();
-  newItem.formattedWaitTime = (newItem.timeCreated).fromNow(true);
-  newMasterItemList.push(newItem);
-  this.setState({masterItemList: newMasterItemList});
-}
 
 render(){
   return (
